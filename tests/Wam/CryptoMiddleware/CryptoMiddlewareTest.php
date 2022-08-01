399ebede3d412fc288a177ce8a241039bf9cd883
welbertymartins <38479002+welbertymartins@users.noreply.github.com>
2022-07-31 21:00:16 -0300

    Update CryptoMiddlewareTest.php

--- tests/Wam/CryptoMiddleware/CryptoMiddlewareTest.php
+++ tests/Wam/CryptoMiddleware/CryptoMiddlewareTest.php
@@ -5,16 +5,18 @@ namespace Test\Wam\CryptoMiddleware;
 use Test\TestCases\BasicTestCase;
 use Wam\CryptoMiddleware\CryptoMiddleware;
 
-class CryptoMiddlewareTest extends BasicTestCase {
-
-    public function testEncodeDecode(): void {
+class CryptoMiddlewareTest extends BasicTestCase
+{
+    public function testEncodeDecode(): void
+    {
         $this->assertEquals(
             'hello world',
             CryptoMiddleware::decode(CryptoMiddleware::encode('hello world'))
         );
     }
-    
-    public function testHash(): void {
+
+    public function testHash(): void
+    {
         $this->assertEquals(
             '309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f',
             CryptoMiddleware::hash('hello world')
